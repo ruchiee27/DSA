b0ace151cc38930523ac8261062f9c56565bf9e3
Ruchi Thakkar <91602701+ruchiee27@users.noreply.github.com>
2021-10-18 21:57:07 +0530

    Add files via upload

---
+++ Data.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n;
+    long int m;
+    char ch ;
+    float b ;
+    double c ;
+    scanf("%d %ld %c %f %lf", &n,&m,&ch,&b,&c);
+    printf("%d\n%ld\n%c\n%f\n%lf",n, m, ch, b, c);
+    return 0;
+}
